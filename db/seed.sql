d1de2f44722b4534de39d55c975b4aeb8145d69b
crockevin <70048195+crockevin@users.noreply.github.com>
2023-11-27 02:57:47 -0500

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,20 +1,20 @@
-INSERT INTO department (id, name) VALUES
-(1, 'HR Department'),
-(2, 'IT Department'),
-(3, 'Finance Department');
+INSERT INTO department (name) VALUES
+('HR Department'),
+('IT Department'),
+('Finance Department');
 
-INSERT INTO role (id, title, salary, department_id) VALUES
-(1, 'HR Manager', 70000.00, 1),
-(2, 'HR Specialist', 50000.00, 1),
-(3, 'IT Manager', 80000.00, 2),
-(4, 'Software Engineer', 60000.00, 2),
-(5, 'Finance Manager', 75000.00, 3),
-(6, 'Financial Analyst', 55000.00, 3);
+INSERT INTO role (title, salary, department_id) VALUES
+('HR Manager', 70000.00, 1),
+('HR Specialist', 50000.00, 1),
+('IT Manager', 80000.00, 2),
+('Software Engineer', 60000.00, 2),
+('Finance Manager', 75000.00, 3),
+('Financial Analyst', 55000.00, 3);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
-(1, 'John', 'Doe', 1, NULL),
-(2, 'Jane', 'Smith', 2, 1),
-(3, 'Mike', 'Johnson', 3, NULL),
-(4, 'Sarah', 'Brown', 4, 3),
-(5, 'David', 'Williams', 5, NULL),
-(6, 'Emily', 'Davis', 6, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+('John', 'Doe', 1, NULL),
+('Jane', 'Smith', 2, 1),
+('Mike', 'Johnson', 3, NULL),
+('Sarah', 'Brown', 4, 3),
+('David', 'Williams', 5, NULL),
+('Emily', 'Davis', 6, 5);
