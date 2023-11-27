b053fa9bc1e251160968963ecfa1f8ef03f01bbb
crockevin <70048195+crockevin@users.noreply.github.com>
2023-11-27 02:58:57 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -11,8 +11,8 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
     PRIMARY KEY(id),
     FOREIGN KEY (department_id) REFERENCES department(id)
@@ -20,9 +20,9 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY(id),
     FOREIGN KEY (role_id) REFERENCES role(id),
